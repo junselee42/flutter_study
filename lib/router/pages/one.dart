e850ef456dac328f947e2680986bc84402679830
junselee42 <junselee@student.42seoul.kr>
2024-02-01 20:32:23 +0900

    update: updated file -첫번째는 플로팅 버튼 만들어서 숫자 카운트 하는 기본예제 구현

--- lib/router/pages/one.dart
+++ lib/router/pages/one.dart
@@ -8,10 +8,31 @@ class OnePage extends StatefulWidget {
 }
 
 class _OnePageState extends State<OnePage> {
+  int _counter = 0;
+
+  void _incrementCounter(){
+    setState(() {
+      _counter++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("위젯00"),),
+      appBar: AppBar(
+        title: Text(widget.toString()),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "버튼을 눌러라"
+            ),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineLarge,)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: _incrementCounter, tooltip: "Increment", child: Icon(Icons.add),),
     );
   }
 }
