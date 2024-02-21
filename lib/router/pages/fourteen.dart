b5c690aba2dfbe8cabcab0ee77b5e1d7be12ef5d
junselee42 <junselee@student.42seoul.kr>
2024-02-21 21:52:06 +0900

    delete: variable -_currentSliderValue 변수 안쓰는거 삭제

--- lib/router/pages/fourteen.dart
+++ lib/router/pages/fourteen.dart
@@ -8,35 +8,33 @@ class FourteenPage extends StatefulWidget {
 }
 
 class _FourteenPageState extends State<FourteenPage> {
-  double _currentSliderValue = 50;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.toString()),
-      ),
-      body: Center(
-        child: Stack(
-          children: [
-            Container(
-              color: Colors.red,
-              width: 300,
-              height: 300,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 200,
-              height: 200,
-            ),
-            Container(
-              color: Colors.green,
-              width: 100,
-              height: 100,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.toString()),
         ),
-      )
-    );
+        body: Center(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.red,
+                width: 300,
+                height: 300,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 200,
+                height: 200,
+              ),
+              Container(
+                color: Colors.green,
+                width: 100,
+                height: 100,
+              ),
+            ],
+          ),
+        ));
   }
 }
