30ce5f3d9fdeb51b89930c9328c59a9d7e86e10a
junselee42 <junselee@student.42seoul.kr>
2024-03-07 16:37:49 +0900

    update: updated ex22

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,8 @@ class _MyHomePageState extends State<MyHomePage> {
           Card(
             child: ListTile(
               leading: const FlutterLogo(),
-              title: const Text('Day23_'),
-              subtitle: const Text('Day23_ twentythird, twentythree'),
+              title: const Text('Day23_테이블'),
+              subtitle: const Text('DataTable 사용하여 테이블 작성하기'),
               hoverColor: Colors.purple,
               onTap: () {
                 Navigator.of(context).pushNamed('/twentythird');
--- lib/router/pages/twentythree.dart
+++ lib/router/pages/twentythree.dart
@@ -8,14 +8,33 @@ class TwentyThreePage extends StatefulWidget {
 }
 
 class _TwentyThreePageState extends State<TwentyThreePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.toString()),
       ),
-      body: Container(),
+      body: Center(
+        child: DataTable(columns: const [
+          DataColumn(label: Text('이름')),
+          DataColumn(label: Text('생년월일'), numeric: true),
+          DataColumn(label: Text('성별')),
+          DataColumn(label: Text('고향')),
+        ], rows: const [
+          DataRow(cells: [
+            DataCell(Text('가나')),
+            DataCell(Text('1990.01.02')),
+            DataCell(Text('남')),
+            DataCell(Text('부산')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('다라')),
+            DataCell(Text('1999.01.02')),
+            DataCell(Text('여')),
+            DataCell(Text('서울')),
+          ])
+        ]),
+      ),
     );
   }
 }
